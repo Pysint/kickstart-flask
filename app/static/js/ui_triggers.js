5dc48644d5f9df12ec73e5e5d098b86790f7b349
Pysint <Pysint@users.noreply.github.com>
2018-11-09 16:32:42 +0100

    Update ui_triggers.js

--- app/static/js/ui_triggers.js
+++ app/static/js/ui_triggers.js
@@ -1,6 +1,8 @@
 // This file should contain all UI triggers ie. clicks and changes
-$("#change-example").on('change', exampleChangeFunction);
-$("#click-example").on('click', exampleClickFunction);
+//$("#change-example").on('change', exampleChangeFunction);
+//$("#click-example").on('click', exampleClickFunction);
+
+// Sidebar actions to load HTML templates.
 $("#sidebar-content").on('click', 'a', function(e){
   e.preventDefault();
   var url = $(this).attr('href');
