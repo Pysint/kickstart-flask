9d396c8f0e2fdbd41e62a57b4cf96df057482caf
Joey Dreijer <joey.dreijer@ing.nl>
2018-11-09 15:57:41 +0100

    Added handlebars example

--- app/static/js/ui_triggers.js
+++ app/static/js/ui_triggers.js
@@ -1,3 +1,8 @@
 // This file should contain all UI triggers ie. clicks and changes
 $("#change-example").on('change', exampleChangeFunction);
 $("#click-example").on('click', exampleClickFunction);
+$("#sidebar-content").on('click', 'a', function(e){
+  e.preventDefault();
+  var url = $(this).attr('href');
+  $('#content-main').load(url);
+});
